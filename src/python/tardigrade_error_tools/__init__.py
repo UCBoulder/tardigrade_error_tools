5f9713458deb43f9c8be84a8a44644a7058cec30
Nathan Miller <nami2227@colorado.edu>
2025-04-29 22:58:30 -0600

    MAINT: Fixed typo in import of setuptools_scm

--- src/python/tardigrade_error_tools/__init__.py
+++ src/python/tardigrade_error_tools/__init__.py
@@ -12,6 +12,6 @@ except PackageNotFoundError:
         import pathlib
         import warnings
         warnings.filterwarnings(action='ignore', message='tag', category=UserWarning, module='setuptools_scm')
-        import setuptools_Scm
+        import setuptools_scm
         __version__ = setuptools_scm.get_version(root=pathlib.Path(__file__).parent.parent.parent.parent.parent)
 
